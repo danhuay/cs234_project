e54cce8dac24d95af993693dfb55372be0d3e1f2
Danhua Yan <danhuayan@gmail.com>
2025-02-18 18:51:53 -0500

    final project staters - half-working

---
+++ code/main.py
@@ -0,0 +1,22 @@
+import retro
+import src.actions as actions
+from src.wrapper import JoypadSpace
+
+def main():
+    env = retro.make(game="SuperMarioBros-Nes", state="Level1-1")
+    env.reset()
+    
+    # Wrap the environment to use our discrete, simple action space
+    # env = JoypadSpace(env, actions.SIMPLE_MOVEMENT)
+    
+    for _ in range(1000000):
+        action = env.action_space.sample()
+        observation, reward, terminated, truncated, info = env.step(action)
+        env.render()
+        if terminated or truncated:
+            env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    main()
