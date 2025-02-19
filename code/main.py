ce0e466e6dda4dfe610911e3abbe4a5e06ff8cd9
Danhua Yan <danhuayan@gmail.com>
2025-02-19 10:37:51 -0500

    update joypad action wrapper

--- code/main.py
+++ code/main.py
@@ -2,13 +2,14 @@ import retro
 import src.actions as actions
 from src.wrapper import JoypadSpace
 
+
 def main():
     env = retro.make(game="SuperMarioBros-Nes", state="Level1-1")
     env.reset()
-    
+
     # Wrap the environment to use our discrete, simple action space
-    # env = JoypadSpace(env, actions.SIMPLE_MOVEMENT)
-    
+    env = JoypadSpace(env, actions.SIMPLE_MOVEMENT)
+
     for _ in range(1000000):
         action = env.action_space.sample()
         observation, reward, terminated, truncated, info = env.step(action)
--- code/src/wrapper.py
+++ code/src/wrapper.py
@@ -1,35 +1,42 @@
 import gymnasium as gym
 from gymnasium.core import ActionWrapper, Env
+import numpy as np
 
 
 class JoypadSpace(ActionWrapper):
     """An environment wrapper to convert binary to discrete action space."""
 
-    # a mapping of buttons to binary values
     _button_map = {
-        'right':  0b10000000,
-        'left':   0b01000000,
-        'down':   0b00100000,
-        'up':     0b00010000,
-        'start':  0b00001000,
-        'select': 0b00000100,
-        'B':      0b00000010,
-        'A':      0b00000001,
-        'NOOP':   0b00000000,
+        "B": 0b100000000,
+        "NOOP": 0b010000000,
+        "SELECT": 0b001000000,
+        "START": 0b000100000,
+        "UP": 0b000010000,
+        "DOWN": 0b000001000,
+        "LEFT": 0b000000100,
+        "RIGHT": 0b000000010,
+        "A": 0b000000001,
     }
 
-    @classmethod
-    def buttons(cls) -> list:
-        """Return the buttons that can be used as actions."""
-        return list(cls._button_map.keys())
+    @staticmethod
+    def array_to_bitmap(action_array):
+        binary_string = "0b"
+        for b in action_array:
+            binary_string += str(b)
+        return binary_string
 
-    def __init__(self, env: Env, actions: list):
+    @staticmethod
+    def bitmap_to_array(action_bitmap):
+        binary_string = bin(action_bitmap)[2:]
+        return np.array([int(b) for b in binary_string], dtype=np.int8)
+
+    def __init__(self, env: Env, allowed_actions: list):
         """
         Initialize a new binary to discrete action space wrapper.
 
         Args:
             env: the environment to wrap
-            actions: an ordered list of actions (as lists of buttons).
+            allowed_actions: an ordered list of actions (as lists of buttons).
                 The index of each button list is its discrete coded value
 
         Returns:
@@ -38,12 +45,12 @@ class JoypadSpace(ActionWrapper):
         """
         super().__init__(env)
         # create the new action space
-        self.action_space = gym.spaces.Discrete(len(actions))
-        # create the action map from the list of discrete actions
-        self._action_map = {}
-        self._action_meanings = {}
+        self.action_space = gym.spaces.Discrete(len(allowed_actions))
+        self._action_map = {}  # discrete action number to array of binary actions
+        self._action_meanings = {}  # discrete action number to action meaning
+
         # iterate over all the actions (as button lists)
-        for action, button_list in enumerate(actions):
+        for action, button_list in enumerate(allowed_actions):
             # the value of this action's bitmap
             byte_action = 0
             # iterate over the buttons in this button list
@@ -51,54 +58,17 @@ class JoypadSpace(ActionWrapper):
                 byte_action |= self._button_map[button]
             # set this action maps value to the byte action value
             self._action_map[action] = byte_action
-            self._action_meanings[action] = ' '.join(button_list)
+            self._action_meanings[action] = ",".join(button_list)
 
-    def step(self, action):
+    def action(self, action: int) -> np.ndarray:
         """
-        Take a step using the given action.
+        Convert a discrete action to a binary action array.
 
         Args:
-            action (int): the discrete action to perform
+            action: the discrete action to convert
 
         Returns:
-            a tuple of:
-            - (numpy.ndarray) the state as a result of the action
-            - (float) the reward achieved by taking the action
-            - (bool) a flag denoting whether the episode has ended
-            - (dict) a dictionary of extra information
+            the binary action array
 
         """
-        # Convert the discrete action to the expected format
-        action_array = [self._action_map[action]]
-        # Take the step and record the output
-        return self.env.step(action_array)
-
-    def reset(self):
-        """Reset the environment and return the initial observation."""
-        return self.env.reset()
-
-    def get_keys_to_action(self):
-        """Return the dictionary of keyboard keys to actions."""
-        # get the old mapping of keys to actions
-        old_keys_to_action = self.env.unwrapped.get_keys_to_action()
-        # invert the keys to action mapping to lookup key combos by action
-        action_to_keys = {v: k for k, v in old_keys_to_action.items()}
-        # create a new mapping of keys to actions
-        keys_to_action = {}
-        # iterate over the actions and their byte values in this mapper
-        for action, byte in self._action_map.items():
-            # get the keys to press for the action
-            keys = action_to_keys[byte]
-            # set the keys value in the dictionary to the current discrete act
-            keys_to_action[keys] = action
-
-        return keys_to_action
-
-    def get_action_meanings(self):
-        """Return a list of actions meanings."""
-        actions = sorted(self._action_meanings.keys())
-        return [self._action_meanings[action] for action in actions]
-
-
-# explicitly define the outward facing API of this module
-__all__ = [JoypadSpace.__name__]
+        return self.bitmap_to_array(self._action_map[action])
